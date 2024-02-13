a946380b5ca68902482a0ec5de90b305cd1ff753
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-02-13 15:09:04 +0100

    add redux provider

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider } from 'react-redux';
+import CourseStack from './Navigations/CourseStack';
+import store from './Redux/Store';
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+       <CourseStack />
+      </NavigationContainer>
+    </Provider>
+      
   );
 }
 
